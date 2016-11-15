7d5b3cd679559c622080368a07efaf8186ff032f
Lucien Mollard <mollard.lucien@gmail.com>
2016-11-15 12:44:23 +0100

    index, show

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115101819) do
+ActiveRecord::Schema.define(version: 20161115103532) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.date     "date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create!(name: "menage", description: "aspirateur et vitres")
+Task.create!(name: "courses", description: "intermarche")
+
